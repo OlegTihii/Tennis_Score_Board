09d33a2264f397c79551ced19778abb8a2f63cab
OlegTihii <YouDoontKnow>
2024-10-01 17:44:56 +0300

    feat. create Hiber session factory

---
+++ src/main/java/com/example/tennisscoreboard/util/HibernateSessionFactory.java
@@ -0,0 +1,13 @@
+package com.example.tennisscoreboard.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactory {
+    private static SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
