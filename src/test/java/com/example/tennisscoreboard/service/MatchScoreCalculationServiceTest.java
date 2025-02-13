547b703ad04ddca4a73cec367d0ab98055478f61
OlegTihii <YouDoontKnow>
2025-02-13 22:28:38 +0300

    test: add new tests

---
+++ src/test/java/com/example/tennisscoreboard/service/MatchScoreCalculationServiceTest.java
@@ -0,0 +1,71 @@
+package com.example.tennisscoreboard.service;
+
+import com.example.tennisscoreboard.dto.MatchDto;
+import com.example.tennisscoreboard.dto.PlayerMatchDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatchScoreCalculationServiceTest {
+
+    private MatchScoreCalculationService matchScoreCalculationService;
+    private MatchDto matchDto;
+    private PlayerMatchDto playerOne;
+    private PlayerMatchDto playerTwo;
+
+
+    @BeforeEach
+    public void beforeAll() {
+        matchScoreCalculationService = new MatchScoreCalculationService();
+        playerOne = PlayerMatchDto.builder().id(1L).playerName("Player_1").build();
+        playerTwo = PlayerMatchDto.builder().id(2L).playerName("Player_2").build();
+
+        matchDto = new MatchDto(playerOne, playerTwo);
+    }
+
+
+    @Test
+    void givenDeuce_whenPlayerOneScores_thenGameNotOver() {
+        playerOne.setPoints(3);
+        playerTwo.setPoints(3);
+
+        matchScoreCalculationService.updateScore(matchDto, playerOne.getId());
+
+        assertEquals(4, playerOne.getPoints());
+        assertEquals(3, playerTwo.getPoints());
+    }
+
+
+    @Test
+    void givenFortyLove_whenPlayerOneScores_thenPlayerOneWinsGame() {
+        playerOne.setPoints(3);
+        playerTwo.setPoints(0);
+
+        matchScoreCalculationService.updateScore(matchDto, playerOne.getId());
+
+        assertEquals(0, playerOne.getPoints());
+
+        assertEquals(0, playerTwo.getPoints());
+
+        assertEquals(1, playerOne.getGames());
+    }
+
+    @Test
+    void givenSixSixInGames_whenPlayerOneScores_thenTiebreakStarts() {
+        playerOne.setGames(6);
+        playerTwo.setGames(6);
+
+        playerOne.setPoints(5);
+        playerTwo.setPoints(4);
+
+        matchScoreCalculationService.updateScore(matchDto, playerOne.getId());
+
+        assertEquals(0, playerOne.getPoints());
+        assertEquals(0, playerTwo.getPoints());
+
+        assertEquals(0, playerOne.getGames());
+        assertEquals(0, playerTwo.getGames());
+        assertEquals(1, playerOne.getSets());
+    }
+}
