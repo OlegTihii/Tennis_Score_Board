519ddfc21f04e60591b123353380f4130f4d57cd
OlegTihii <YouDoontKnow>
2025-02-18 22:49:01 +0300

    test: add tests for FinishedMatchesPersistenceServiceTest.java

--- src/main/java/com/example/tennisscoreboard/service/OngoingMatchesService.java
+++ src/main/java/com/example/tennisscoreboard/service/OngoingMatchesService.java
@@ -5,7 +5,6 @@ import com.example.tennisscoreboard.dto.PlayerDto;
 import com.example.tennisscoreboard.entity.Player;
 import com.example.tennisscoreboard.exception.MatchAlreadyInProgressException;
 import com.example.tennisscoreboard.mapper.PlayerMapper;
-import com.example.tennisscoreboard.repository.PlayerRepository;
 
 import java.util.Map;
 import java.util.UUID;
@@ -29,7 +28,7 @@ public class OngoingMatchesService {
         // Проверка есть ли игроки в бд и добавление
         // Проверка играет ли новый добавленный игрок матч. Один и тот же игрок не может играть 2 матча одновременно
 
-        playerPersistenceService.setPlayerRepository(new PlayerRepository());
+        //  playerPersistenceService.setPlayerRepository(new PlayerRepository());
 
         Player playerOne = playerPersistenceService.findOrSave(playerOneDto);
         Player playerTwo = playerPersistenceService.findOrSave(playerTwoDto);
---
+++ src/test/java/com/example/tennisscoreboard/service/OngoingMatchesServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.tennisscoreboard.service;
+
+import org.junit.jupiter.api.Test;
+
+class OngoingMatchesServiceTest {
+
+    @Test
+    void test() {
+
+    }
+
+}
